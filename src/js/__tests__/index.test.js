e17a54f4fd26d65480f7d5d013f34d3d353fbd1f
MaxHugoboss <m.gorelov.91@yandex.ru>
2024-04-17 21:25:42 +0500

    init

---
+++ src/js/__tests__/index.test.js
@@ -0,0 +1,18 @@
+import { getHealthStatus } from '../index';
+
+describe('getHealthStatus function', () => {
+  test('should return "healthy" if health is more than 50', () => {
+    const character = { name: 'Маг', health: 90 };
+    expect(getHealthStatus(character)).toBe('healthy');
+  });
+
+  test('should return "wounded" if health is between 15 and 50', () => {
+    const character = { name: 'Маг', health: 30 };
+    expect(getHealthStatus(character)).toBe('wounded');
+  });
+
+  test('should return "critical" if health is less than 15', () => {
+    const character = { name: 'Маг', health: 10 };
+    expect(getHealthStatus(character)).toBe('critical');
+  });
+});
